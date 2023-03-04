8ce2b6f5a46c5499a12c95b7030e7c7c954ee27f
1tsukoyumi <lh.honguyenthanh@gmail.com>
2023-03-04 17:41:33 +0700

    chỉnh sửa và thêm tính năng cho login

--- login/login.js
+++ login/login.js
@@ -1,10 +1,37 @@
-const ipn = document.querySelector('.input-pass')
-const btn = document.querySelector('.show-btn')
+const ipn = document.querySelector(".input-pass");
+const btn = document.querySelector(".show-btn");
 
-btn.addEventListener('click',function() {
-    const currentType = ipn.getAttribute('type')
-    ipn.setAttribute(
-        'type',
-        currentType === 'password' ? 'text' : 'password'
-    )
-})
+// ẩn hiện pass
+btn.addEventListener("click", function () {
+  const currentType = ipn.getAttribute("type");
+  ipn.setAttribute("type", currentType === "password" ? "text" : "password");
+});
+
+//bắt lỗi đăng nhập
+$("#btn-login").click(function () {
+  //lấy thông tin user
+  var data_login = {};
+  data_login.user = $("#input-user").val();
+  data_login.pass = $("#input-pass").val();
+
+  //gửi yêu cầu đến api
+  $.ajax({
+    URL: '#',
+    type: 'post',
+    contenttype: 'application/json',
+    data: JSON.stringify(data_login),
+    success: function(response) {
+        //đăng nhập thành công
+        if (response.message === "true") {
+            console.log('Đăng nhập thành công!');
+        } 
+        //đăng nhập thất bại
+        else {
+            console.error('Sai tài khoản hoặc mật khẩu');
+        }
+    },
+    error: function(error) {
+        console.error('Đăng nhập không thành công!', error);
+    }
+  });
+});
